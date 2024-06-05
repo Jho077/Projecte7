fb25c9d3ac2ea6618f2877155127bc5c4bc1168f
Jho077 <jespinoza@cendrassos.net>
2024-06-05 15:10:58 +0200

    feature_test_php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,8 +1,52 @@
 <?php
 
-use function Pest\Laravel\get;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Http\UploadedFile;
 
-test('example', function () {
-    get('/')->assertStatus(200);
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
 
-});
+    public function testUserCanCreate()
+    {
+        // Crear un usuario de prueba
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+    
+        //POST de inicio de sesión
+        $response = $this->post('/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+    
+        // Verifica que el user se fue a welcome o /
+        $response->assertRedirect('/');
+    
+        // User autenticado
+        $this->assertAuthenticated();
+    
+        // Hacemos el get para la vista
+        $createRestaurantViewResponse = $this->get('/restaurants/create');
+
+        // La vista se carga bien
+        $createRestaurantViewResponse->assertStatus(200);
+
+        // Crear el restaurant
+        $restaurantData = [
+            'name' => 'Nombre del restaurante',
+            'address' => 'Dirección del restaurante',
+            'latitude' => 12.3651200,
+            'longitude' => 12.3651200,
+            'description' => 'Descripción del restaurante',
+            'image' => UploadedFile::fake()->image('restaurant.jpg'),
+        ];
+    
+        $createRestaurantResponse = $this->post('/restaurants', $restaurantData);
+       
+        $createRestaurantResponse->assertRedirect('/misRestaurantes');
+    }
+}
